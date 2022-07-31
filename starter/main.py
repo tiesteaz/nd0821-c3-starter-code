c9bf9d7ce2cfe930c8f4399ee94e92f20cf23801
Andrey Baranov <tiesteaz@gmail.com>
2022-07-31 19:05:17 -0400

    added Procfile for Heroku deployment, removed pycache files, added code in main.py to allow Heroku to use DVC

--- starter/main.py
+++ starter/main.py
@@ -1,14 +1,70 @@
 # Put the code for your API here.
+from operator import index
+import joblib
 from typing import Union
+from pandas.core.frame import DataFrame
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starter.ml.data import process_data
 
+#from fastapi.responses import PlainTextResponse
+
+# Required for Heroku
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 # Instantiate the app
 app = FastAPI()
 
 class MyClass(BaseModel):
     body: str
+
+
+class ModelRequest(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
+
+    class Config:
+            schema_extra = {
+                "example": {
+                    "age": 44,
+                    "workclass": "private",
+                    "fnlgt": 198282,
+                    "education": "Bachelors",
+                    "education-num": 9,
+                    "marital-status": "Married-civ-spouse",
+                    "occupation": "Craft-repair",
+                    "relationship": "Wife",
+                    "race": "White",
+                    "sex": "Female",
+                    "capital-gain": 5180,
+                    "capital-loss": 0,
+                    "hours-per-week": 40,
+                    "native-country": "Poland"
+                    }
+                }
+            
+            @classmethod
+            def alias_generator(cls, string: str) -> str:
+                return string.replace('_', '-')
+
 
 # Define a GET on the specified endpoint
 @app.get("/")
@@ -19,3 +75,49 @@ async def say_greeting():
 @app.post("/{path}")
 async def insert_item(obj: MyClass, path: str, query: str):
     return {"path": path, "body": obj.body, "query": query}
+
+
+@app.post("/inference/")
+async def perform_inference(request_data: ModelRequest):
+    
+    request_dictionary = request_data.dict(by_alias=True)
+    input_data = DataFrame(request_dictionary, index=[0])
+
+    print(input_data)
+
+    # load trained models
+    model = joblib.load("model/TrainedRandomForestModel.joblib")
+    encoder = joblib.load("model/TrainedOneHotEncoder.joblib")
+    binarizer = joblib.load("model/TrainedLabelBinarizer.joblib")
+    
+    # load list of categorical features
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    processed_input_data, _, _, _ = process_data(
+        input_data,
+        categorical_features = cat_features,
+        training = False,
+        encoder = encoder,
+        lb = binarizer
+    )
+
+    salary_prediction = model.predict(processed_input_data)
+
+    salary_binary = salary_prediction[0]
+
+    if salary_binary == 1:
+        salary_label = ">$50K"
+    elif salary_binary == 0:
+        salary_label = "<=$50K"
+
+
+    return {"salary": salary_label }
